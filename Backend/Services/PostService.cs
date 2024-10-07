c62f2963a68458823d97c994b432eb67e4dc4c2b
Adrian Bañuelas <adriangpe5666@gmail.com>
2024-10-07 15:00:29 -0600

    Http client with dependency injection and appsettings handle added

---
+++ Backend/Services/PostService.cs
@@ -0,0 +1,33 @@
+﻿using Backend.DTOs;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace Backend.Services
+{
+    public class PostService : IPostService
+    {
+        private HttpClient _httpClient;
+
+        public PostService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<PostDto>> Get()
+        {
+            var result = await _httpClient.GetAsync(_httpClient.BaseAddress);
+            var body = await result.Content.ReadAsStringAsync();
+
+            if (string.IsNullOrEmpty(body))
+            {
+                throw new ArgumentNullException("Body was empty.");
+            }
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            IEnumerable<PostDto> post = JsonSerializer.Deserialize<IEnumerable<PostDto>>(body, options)!;
+
+            return post;
+        }
+    }
+}
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ba7bec5cfbd3d8e32a328631cad907134f7d91a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca5c18355687e112c04bd50bb83f8c69acd724f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
